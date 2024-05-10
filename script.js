4ec96e198bc9ee259d4c09da320941de98123dda
Jennifer Acosta <77638204+jennawlin3@users.noreply.github.com>
2024-05-10 15:53:14 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const d = document;
+const share_btn1 = d.querySelector("#share-btn1");
+const share_btn2 = d.querySelector("#share-btn2");
+const share_btnd = d.querySelector(".share-btn_desk");
+const showContainer = d.querySelector(".share-container");
+const hideContainer = d.querySelector(".profile-container");
+const tooltip = d.querySelector(".container-share_desk");
+
+console.log(hideContainer)
+console.log(screen.width)
+console.log(screen.availWidth)
+
+
+if(window.matchMedia('(min-width: 800px)')) {
+d.addEventListener("click", (e) => {
+    console.log(e.target);
+
+    if(e.target === share_btn1) {
+        showContainer.classList.toggle("hide");
+        hideContainer.classList.toggle("hide");
+    }
+    if(e.target === share_btn2) {
+        showContainer.classList.toggle("hide");
+        hideContainer.classList.toggle("hide");
+    }
+})
+}
+
+if(window.matchMedia('(min-width: 801px)')) {
+    d.addEventListener("click", (e) => {
+        console.log(e.target);
+    
+        if(e.target === share_btnd) {
+            tooltip.classList.toggle("disappear");
+        }
+    })
+}
